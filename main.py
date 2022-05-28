2b26bd9280169568f753040b729e8ff9ce20ce2e
PsalmogunsGRT <105373542+PsalmogunsGRT@users.noreply.github.com>
2022-05-28 07:04:53 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-# Check if two words are anagrams 
+# Read text from a file, and count the occurence of words in that text
 # Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def find_anagram(word, anagram)
+def read_file_content(filename):
+    # [assignment] Add your code here 
+     f = open(filename, "r")
+     content = f.read()
+     print(content)
+     f.close()
+    
+
+
+def count_words():
+    text = read_file_content("./story.txt")
     # [assignment] Add your code here
-first_string = input("Provide the first string: ")
-second_string = input("Provide the second string: ") 
+text_list = text.split()
+dict_count = {}
 
-if sorted(first_string) == sorted(second_string):
-    print("The two strings are anagrams.")
-else:
-    print("The two strings are not anagrams.") 
+  for txt in text_list:
+          if txt not in dict_count:
+            dict_count[txt] = 1
+
+         else:
+            dict_count[txt] = dict_count[txt] + 1
+          
+          
+ return dict_count
+
+
+
+print(count_words())
+
+
+
+    
